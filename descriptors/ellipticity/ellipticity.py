14636ecf10ac010a8cb3df66223be082d9752151
jacek789 <jacek.h@vp.pl>
2018-07-26 16:04:24 +0200

    fix for ellipticity function

--- descriptors/ellipticity/ellipticity.py
+++ descriptors/ellipticity/ellipticity.py
@@ -1,9 +1,9 @@
-from math import pi, sqrt
+from math import pi
 # from descriptors.utils.moments.moments import moments
 from descriptors.utils.moments import moments
 
 
-def ellipticity(image, method='moment_invariants_sqrt'):
+def ellipticity(image, method='moment_invariants'):
     """
     There are 4 methods to define ellipticity:
     Moment invariants (in two similar versions): E_I
@@ -14,24 +14,18 @@ def ellipticity(image, method='moment_invariants_sqrt'):
 
     This function implements the moment invariants so far.
     :param image: np.ndarray, binary mask
-    :param method: method
     :return: float \in [0, 1]
     """
-    if method == 'moment_invariants_sqrt':
+    if method == 'moment_invariants':
         m = moments(image)
         # I_1 affine moment invariant
         i_1 = (m['mu20'] * m['mu02'] - m['mu11'] ** 2) / m['mu00'] ** 4
 
-        ic = 16 * pi ** 2
-        # ic2 = 1 / ic  # I_1 for unit circle
-        return sqrt(ic * i_1)
-    elif method == 'moment_invariants':
-        m = moments(image)
-        # I_1 affine moment invariant
-        i_1 = (m['mu20'] * m['mu02'] - m['mu11'] ** 2) / m['mu00'] ** 4
-
-        ic = 16 * pi ** 2
-        # ic2 = 1 / ic  # I_1 for unit circle
-        return ic * i_1
+        # I_1 for unit circle
+        ic = 1 / (16 * pi ** 2)
+        if i_1 <= ic:
+            return i_1 / ic
+        else:
+            return ic / i_1
     else:
         print("Wrong method.")
